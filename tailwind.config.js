b1874fd732f2ce80880fdb7722f9ea87d551e908
Ebe <edememediong1@gmail.com>
2023-04-06 23:26:50 +0100

    Resilience is fun

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,css}"],
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js,css}',
+  ],
   theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
     extend: {},
   },
   plugins: [],
